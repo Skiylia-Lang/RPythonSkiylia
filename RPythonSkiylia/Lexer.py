1cd20759dcdd056bc3d32c262dbfab87e2ef1ddb
Skiy <skiy@outlook.com>
2021-04-11 00:37:58 +0100

    lexer creates more interesting tokens!

--- RPythonSkiylia/Lexer.py
+++ RPythonSkiylia/Lexer.py
@@ -8,7 +8,26 @@ class Lexer(Utils.Scan):
         #set the position elements to 1 at start
         self.line = self.char = 1
         #set the lexer elements to 0 at start
-        self.current = self.indent = 0
+        self.start = self.indent = 0
+        #dictionary of tokens
+        self.Tokendict = {"(":"LeftParen", ")":"RightParen", ":":"Colon",
+                          ",":"Comma", ".":"Dot", "&":"And", "|":"Or", "^":"Xor",
+                          #dual (or single) character tokens
+                          "-": {"-":"MinusMinus", "":"Minus"},
+                          "+": {"+":"PlusPlus", "":"Plus"},
+                          "*": {"*":"StarStar", "":"Star"},
+                          ">": {"=":"EGreater", "":"Greater"},
+                          "<": {"=":"ELess", "":"Less"},
+                          "?": {":":"QColon", "?":"QQuestion", "":"Question"},
+                          # 'n' character tokens
+                          "=": {"=": {"=":"EEEqual", "":"EEqual"}, "":"Equal"},
+                          "~": {"~": {"~":"Fuzequal"}, },
+                          "!": {"=": {"=": "NEEqual", "":"NEqual"}, "~": {"~": "NFuzequal", }, "":"Not"}, }
+        #set of all keywords
+        self.keywordset = {"and", "break", "continue", "class", "def", "do", "elif", "else", "false", "for", "if", "import", "in",
+                           "not", "null", "or", "return", "self", "super", "true", "var", "where", "while", "xor", }
+        #with a special dictionary if certain keywords need to map to something else
+        self.keywordmap = {"when": "where", }
 
     def scan(self, source):
         #initialise the Utils.scan
@@ -18,7 +37,9 @@ class Lexer(Utils.Scan):
         while not self.atEnd():
             #fetch the next token
             tok = self.fetchToken()
+            #if we returned a token
             if tok:
+                #add it to our token list
                 self.tokens.append(tok)
             #update our starting position
             self.start = self.current
@@ -29,30 +50,66 @@ class Lexer(Utils.Scan):
     def fetchToken(self):
         #fetch the next character
         thischar = self.next()
-        #dictionary of tokens
-        Tokendict = {"(":"LeftParen", ")":"RightParen", ":":"Colon",
-                     ",":"Comma", ".":"Dot", "&":"And", "|":"Or", "^":"Xor",
-                     #dual (or single) character tokens
-                     "-": {"-":"MinusMinus", "":"Minus"},
-                     "+": {"+":"PlusPlus", "":"Plus"},
-                     "*": {"*":"StarStar", "":"Star"},
-                     ">": {"=":"EGreater", "":"Greater"},
-                     "<": {"=":"ELess", "":"Less"},
-                     "?": {":":"QColon", "?":"QQuestion", "":"Question"},
-                     # 'n' character tokens
-                     "=": {"=": {"=":"EEEqual", "":"EEqual"}, "":"Equal"},
-                     "~": {"~": {"~":"Fuzequal"}, },
-                     "!": {"=": {"=": "NEEqual", "":"NEqual"}, "~": {"~": "NFuzequal", }, "":"Not"}, }
         #check if this character matches an entry in the token dict
-        if thischar in Tokendict:
-            return self.fromdict(Tokendict, thischar)
-        elif thischar == " ":
+        if thischar in self.Tokendict:
+            return self.fromdict(thischar), thischar
+        #check for comments (they need slightly different logic)
+        elif thischar == "/":
+            if self.match("/"):
+                #check for '///' in a row
+                if self.match("/"):
+                    #keep incrementing until we meet another '///' cluster
+                    while not self.matchmul("/","/","/"):
+                        self.next()
+                #else, we only had '//'
+                else:
+                    #keep incrementing until we meet a line break
+                    while not match("\n"):
+                        self.next()
+                #and return the comment as an ignorable token I guess
+                return "Comment", self.joined(self.start)
+            #otherwise, we just have division
+            else:
+                return "Slash", thischar
+        #otherwise, check for numbers / identifiers
+        else:
+            if self.numeric(thischar):
+                return "Number", self.asnumber()
+            elif self.alpha(thischar):
+                return "Identifier", self.asidentifier()
             return
 
+    def asnumber(self):
+        while self.numeric(self.peek()):
+            self.next()
+        if self.match("."):
+            while self.numeric(self.peek()):
+                self.next()
+        return self.joined(self.start)
+
+    def asidentifier(self):
+        while self.alphanumeric(self.peek()):
+            self.next()
+        ident = self.joined(self.start)
+        if ident in self.keywordset:
+            ident = ident.capitalize()
+        elif ident in self.keywordmap:
+            ident = self.keywordmap.get(ident).capitalize()
+        return ident
+
+    def numeric(self, char):
+        return "0" <= char <= "9"
+
+    def alpha(self, char):
+        return ("a" <= char <= "z") or ("A" <= char <= "Z") or char == "_"
+
+    def alphanumeric(self, char):
+        return ("0" <= char <= "9") or ("a" <= char <= "z") or ("A" <= char <= "Z") or char == "_"
+
     #fetch the next token if it is in the dictionary supplied
-    def fromdict(self, dic, char):
+    def fromdict(self, char):
         #fetch the value at the character key
-        this = dic.get(char)
+        this = self.Tokendict.get(char)
         #if that was a dictionary
         while isinstance(this, dict):
             #fetch the next character
@@ -71,6 +128,6 @@ class Lexer(Utils.Scan):
         return this
 
 lexer = Lexer()
-source="print(5 + 4)\0"
+source="///this is a test///print(5 + 4 and 3 when x > 1)\0"
 toks = lexer.scan(source)
 print(toks)
--- RPythonSkiylia/Utils.py
+++ RPythonSkiylia/Utils.py
@@ -15,10 +15,40 @@ class Scan:
             return self.source[self.current+offset]
         return self.source[self.current]
 
+    #check if the next object matches an expectation, and increment if it does
+    def match(self, obj):
+        if self.peek() == obj:
+            self.current+=1
+            return True
+        return False
+
+    def matchmul(self, *objs):
+        if self.group(self.current + len(objs)) == list(objs):
+            self.current+=len(objs)
+            return True
+        return False
+
     #fetch the current object, and increment
     def next(self):
         self.current+=1
         return self.source[self.current-1]
+
+    #fetch the objects between two indices as a list
+    def group(self, idxa, idxb=""):
+        #if we are only given one index
+        if not idxb:
+            #then use the current pointer location
+            idxb=self.current
+            #if the given index is larger than the current
+            if (idxa > idxb):
+                #then return from the current to that index
+                return self.source[idxb : idxa]
+        #else return between the two
+        return self.source[idxa : idxb]
+
+    #fetch the objects between two indices, but as a string
+    def joined(self, idxa, idxb=""):
+        return "".join(self.group(idxa, idxb))
 
     #check if we have reached the end of the object itterables
     def atEnd(self, offset=0):
